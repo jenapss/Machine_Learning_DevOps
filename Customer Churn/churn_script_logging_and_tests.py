18c2bc85cc48bfe51d9a0acf820b2f8093dd9f24
Jelaleddin Sultanov <jelaleddinsultanov@yandex.ru>
2021-09-13 09:40:40 +0500

    testing is above pylint 7 score

--- Customer Churn/churn_script_logging_and_tests.py
+++ Customer Churn/churn_script_logging_and_tests.py
@@ -7,124 +7,114 @@ MODEL_PATH = 'Customer Churn/models'
 
 logging.basicConfig(
     filename='Customer Churn/logs/churn_library.log',
-    level = logging.INFO,
+    level=logging.INFO,
     filemode='w',
     format='%(name)s - %(levelname)s - %(message)s')
 
+
 def test_import():
-	'''
-	test data import - this example is completed for you to assist with the other test functions
-	'''
+    '''
+    test data import - this example is completed for you to assist with the other test functions
+    '''
 
-	try:
-		df = cls.import_data("Customer Churn/data/bank_data.csv")
-		logging.info("SUCCESS - Testing import_data")
-	except FileNotFoundError as err:
-		logging.error("Testing import_eda: The file wasn't found")
-		raise err
+    try:
+        df = cls.import_data("Customer Churn/data/bank_data.csv")
+        logging.info("SUCCESS - Testing import_data")
+    except FileNotFoundError as err:
+        logging.error("Testing import_eda: The file wasn't found")
+        raise err
 
-	try:
-		assert df.shape[0] > 0
-	except AssertionError as err:
-		logging.error("Testing import_data: The file doesn't appear to have rows and columns")
-		raise err
+    try:
+        assert df.shape[0] > 0
+    except AssertionError as err:
+        logging.error(
+            "Testing import_data: The file doesn't appear to have rows and columns")
+        raise err
 
 
 def test_eda():
-	'''
-	test perform eda function
-	'''
-	df = cls.import_data("Customer Churn/data/bank_data.csv")
-	cls.perform_eda(df)
-	assert os.path.exists(IMG_PATH + 'churn_dist.jpg') == True
-	assert os.path.exists(IMG_PATH + 'customer_age.jpg') == True
-	assert os.path.exists(IMG_PATH + 'martial_status_dist.jpg') == True
-	assert os.path.exists(IMG_PATH + 'total_transaction_dist.jpg') == True
-	assert os.path.exists(IMG_PATH + 'heatmap.jpg') == True
-	
-	
+    '''
+    test perform eda function
+    '''
+    df = cls.import_data("Customer Churn/data/bank_data.csv")
+    cls.perform_eda(df)
+    assert os.path.exists(IMG_PATH + 'churn_dist.jpg')
+    assert os.path.exists(IMG_PATH + 'customer_age.jpg')
+    assert os.path.exists(IMG_PATH + 'martial_status_dist.jpg')
+    assert os.path.exists(IMG_PATH + 'total_transaction_dist.jpg')
+    assert os.path.exists(IMG_PATH + 'heatmap.jpg')
 
 
 def test_encoder_helper():
-	'''
-	test encoder helper
-	'''
-	try:
-		df = cls.import_data('Customer Churn/data/bank_data.csv')
-		category_lst = [
-			'Gender',
-			'Income_Category',
-			'Marital_Status',
-			'Education_Level',
-			'Card_Category']
-		updated_df = cls.encoder_helper(df, category_lst)
-		print(type(updated_df))
-		assert (category_lst[0] + '_Churn' in updated_df) == True
-		assert (category_lst[1] + '_Churn' in updated_df) == True
-		assert (category_lst[2] + '_Churn' in updated_df) == True
-		assert (category_lst[3] + '_Churn' in updated_df) == True
-		assert (category_lst[4] + '_Churn' in updated_df) == True
-		logging.info('SUCCESS - Testing encoder_helper: Columns are created!')
-	except AssertionError as err:
-		logging.error("ERROR - Testing encoder_helper: Columns are not created!")
-		raise err
+    '''
+    test encoder helper
+    '''
+    try:
+        df = cls.import_data('Customer Churn/data/bank_data.csv')
+        category_lst = [
+            'Gender',
+            'Income_Category',
+            'Marital_Status',
+            'Education_Level',
+            'Card_Category']
+        updated_df = cls.encoder_helper(df, category_lst)
+        print(type(updated_df))
+        assert (category_lst[0] + '_Churn' in updated_df)
+        assert (category_lst[1] + '_Churn' in updated_df)
+        assert (category_lst[2] + '_Churn' in updated_df)
+        assert (category_lst[3] + '_Churn' in updated_df)
+        assert (category_lst[4] + '_Churn' in updated_df)
+        logging.info('SUCCESS - Testing encoder_helper: Columns are created!')
+    except AssertionError as err:
+        logging.error(
+            "ERROR - Testing encoder_helper: Columns are not created!")
+        raise err
+
 
 def test_perform_feature_engineering():
-	'''
-	test perform_feature_engineering
-	'''
-	
-	df = cls.import_data('Customer Churn/data/bank_data.csv')
-	category_lst = [
-		'Gender',
-		'Income_Category',
-		'Marital_Status',
-		'Education_Level',
-		'Card_Category']
-	updated_df = cls.encoder_helper(df, category_lst)
-	X_train, X_test, y_train, y_test = cls.perform_feature_engineering(updated_df)
-	try:
-		assert X_train.shape[0] > 0
-		assert X_train.shape[1] == 19 
-		assert X_test.shape[0] > 0
-		assert X_test.shape[1] == 19 
-		assert y_train.shape[0] > 0
-		assert y_test.shape[0] > 0
-		logging.info("SUCCESS - Feature Engineering")
-	except AssertionError as err:
-		logging.error("ERROR - Feature Engineering")
-		raise err
-		
+    '''
+    test perform_feature_engineering
+    '''
 
+    df = cls.import_data('Customer Churn/data/bank_data.csv')
+    category_lst = [
+        'Gender',
+        'Income_Category',
+        'Marital_Status',
+        'Education_Level',
+        'Card_Category']
+    updated_df = cls.encoder_helper(df, category_lst)
+    X_train, X_test, y_train, y_test = cls.perform_feature_engineering(
+        updated_df)
+    try:
+        assert X_train.shape[0] > 0
+        assert X_train.shape[1] == 19
+        assert X_test.shape[0] > 0
+        assert X_test.shape[1] == 19
+        assert y_train.shape[0] > 0
+        assert y_test.shape[0] > 0
+        logging.info("SUCCESS - Feature Engineering")
+    except AssertionError as err:
+        logging.error("ERROR - Feature Engineering")
+        raise err
 
 
 def test_train_models():
-	'''
-	test train_models
-	'''
-	try:
-		assert os.path.exists(MODEL_PATH + '/rfc_model.pkl') == True
-		assert os.path.exists(MODEL_PATH + '/logistic_model.pkl') == True
-		logging.info('SUCCESS - MODELS SAVED')
-	except AssertionError as err:
-		logging.error("ERROR - MODELS NOT SAVED")
-		raise err
-
-
- 
+    '''
+    test train_models
+    '''
+    try:
+        assert os.path.exists(MODEL_PATH + '/rfc_model.pkl')
+        assert os.path.exists(MODEL_PATH + '/logistic_model.pkl')
+        logging.info('SUCCESS - MODELS SAVED')
+    except AssertionError as err:
+        logging.error("ERROR - MODELS NOT SAVED")
+        raise err
 
 
 if __name__ == "__main__":
-	test_import()
-	test_eda()
-	test_encoder_helper()
-	test_perform_feature_engineering()
-	test_train_models()
-
-
-
-
-
-
-
-
+    test_import()
+    test_eda()
+    test_encoder_helper()
+    test_perform_feature_engineering()
+    test_train_models()
